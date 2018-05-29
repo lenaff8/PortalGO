385a3ff23aed2acfbf8e0bde882d4aa7a1f1c261
lenaff8 <lenaff8@users.noreply.github.com>
2018-05-29 20:01:43 +0200

    !deleted

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float moveTime = 0.1f;
+
+    private
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MoveEnemy()
+    {
+        /*int xDir = 0;
+        int yDir = 0;
+
+        if (Mathf.Abs(target.position.x - transform.position.x) < float.Epsilon)
+
+            //If the y coordinate of the target's (player) position is greater than the y coordinate of this enemy's position set y direction 1 (to move up). If not, set it to -1 (to move down).
+            yDir = target.position.y > transform.position.y ? 1 : -1;
+
+        //If the difference in positions is not approximately zero (Epsilon) do the following:
+        else
+            //Check if target x position is greater than enemy's x position, if so set x direction to 1 (move right), if not set to -1 (move left).
+            xDir = target.position.x > transform.position.x ? 1 : -1;
+
+        //Call the AttemptMove function and pass in the generic parameter Player, because Enemy is moving and expecting to potentially encounter a Player
+        AttemptMove<Player>(xDir, yDir);*/
+    }
+}
