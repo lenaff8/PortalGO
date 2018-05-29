4195c78dcd2f854a61665b435906665e94e22995
ralucado <ralucado@users.noreply.github.com>
2018-05-30 01:21:36 +0200

    Button added
    Left click to activate, right click to deactivate

---
+++ Assets/Scripts/ButtonPress_Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress_Script : MonoBehaviour {
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Active");
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            anim.SetTrigger("Deactive");
+        }
+    }
+}
