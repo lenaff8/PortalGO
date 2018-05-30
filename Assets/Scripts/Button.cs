8a336f284ac8791e1ede7107c27efbcaf1764c64
ralucado <ralucado@users.noreply.github.com>
2018-05-30 22:25:44 +0200

    Added Door. Button triggers GameObjects.

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    Animator anim;
+    public  Animator otherObject;
+    // Use this for initialization
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        anim.SetTrigger("Active");
+        otherObject.SetTrigger("Activate");
+    }
+    void OnTriggerExit(Collider other)
+    {
+        anim.SetTrigger("Deactive");
+        otherObject.SetTrigger("Deactivate");
+    }
+
+}
